61f2776822bf79ed29a96955ffa44c2ba45373bc
Tarik B Deliktas <tarikdeliktas@gmail.com>
2023-10-12 22:39:04 -0700

    user agent setup

--- scraper.ts
+++ scraper.ts
@@ -1,16 +1,22 @@
 const playwright = require('playwright')
+const random_useragent = require('random-useragent')
 
 const BASE_URL = 'https://www.saucedemo.com/'
 
     ;(async () => {
+        // Create Random Agent
+        const agent = random_useragent.getRandom()
         // Setup browser
         const browser = await playwright.chromium.launch({ headless: true })
-        const context = await browser.newContext()
+        const context = await browser.newContext({ userAgent: agent })
         const page = await context.newPage({ bypassCSP: true })
         await page.setDefaultTimeout(30000)
         await page.setViewportSize({ width: 800, height: 600 })
         await page.goto(BASE_URL)
 
+        // Get Data from Website
+
+        // Store Data into File
         // Close browser
         await browser.close()
     })().catch(error => {
