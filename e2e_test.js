82bd76e3510f5604fd11afd91e8183a876a50196
Tarik B Deliktas <tarikdeliktas@gmail.com>
2023-10-12 21:52:03 -0700

    added hooks to spec file

--- e2e_test.js
+++ e2e_test.js
@@ -1,11 +1,17 @@
 Feature("Swag Lab E2E Tests")
 
-Scenario("Login Test - Negative", ({ I }) => {
+Before(({ I }) => {
     I.amOnPage('https://www.saucedemo.com/')
+})
+
+After(({ I }) => {
+    console.log("after hook")
+})
+
+Scenario("Login Test - Negative", ({ I }) => {
     I.seeElement('#login_button_container')
     I.fillField('[data-test="username"]', 'invalid_username')
     I.fillField('[data-test="password"]', 'invalid_password')
     I.click('[data-test="login-button"]')
     I.seeElement('[data-test="error"]')
-
 })
