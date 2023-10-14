1823187aa3f05c5746b6cb498db41db1afc60a82
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-10-13 21:18:33 -0700

    first test for mocha framework

---
+++ tests/mocha.spec.js
@@ -0,0 +1,26 @@
+const { chromium } = require("playwright")
+const expect = require("chai").expect
+
+let browser
+let page
+
+before(async () => {
+    browser = await chromium.launch()
+})
+
+after(async () => {
+    await browser.close()
+})
+
+beforeEach(async () => {
+    page = await browser.newPage()
+})
+
+afterEach(async () => {
+    await page.close()
+})
+
+it("TC1 Playwright Mocha test", async() => {
+    await page.goto("https://www.saucedemo.com/")
+    expect('.login_logo').to.exist
+})
